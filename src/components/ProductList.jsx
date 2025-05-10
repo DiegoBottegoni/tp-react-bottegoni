007af0572b5b125cbb439c1f2885658f9de0344e
DiegoBottegoni <diego.bottegoni@gmail.com>
2025-05-10 11:28:09 -0300

    tailwind installed && first products fetch

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 function ProductList() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((data) => setProducts(data))
+            .catch((err) => console.error("Error fetching products:", err));
+    }, []);
+
     return (
-        <div>ProductList</div>
-    )
+        <div>
+            {products.map((product) => (
+                <p key={product.id} className="mb-10">{product.title}</p>
+            ))}
+        </div>
+    );
 }
 
-export default ProductList
+export default ProductList;
